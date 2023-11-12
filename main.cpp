381d59e562a90230178b38f750dddaa60209d6f8
Ajmappas <73444776+Ajmappas@users.noreply.github.com>
2023-11-12 11:40:52 +0200

    Update 0.2

--- main.cpp
+++ main.cpp
@@ -2,53 +2,46 @@
 #include <string>
 #include <vector>
 #include <stdlib.h>
+#include <Windows.h>
+#include <winuser.h>
 
-char nums[] = "7391";
+std::string nums;
 std::vector<int> number_buffer;
 
 std::string end_num = "";
 
 int main(){
-  for(auto n : nums){
+
+  std::cout << "Type 4 digits you desire to sort: ";
+  std::cin >> nums;
+
+  for(auto& n : nums){
     if(n != 0){
       int a = (int)n - 48;
       number_buffer.push_back(a);
     }
   }
+  
+  //Checks the middle numbers.
+  if(number_buffer[1] < number_buffer[1]){}
+  else if(number_buffer[1] > number_buffer[2]){ std::swap(number_buffer[1], number_buffer[2]); }
 
-  for(int i = 0; i < 2; i++){
-    if(number_buffer[0] < number_buffer[1]){
+  //Checks the first with the last number.
+  if(number_buffer[0] < number_buffer[3]){}
+  else if(number_buffer[0] > number_buffer[3]){ std::swap(number_buffer[0], number_buffer[3]); }
+    
+  //Checks the first two numbers.
+  if(number_buffer[0] < number_buffer[1]){} 
+  else if(number_buffer[0] > number_buffer[1]){ std::swap(number_buffer[0], number_buffer[1]); } 
 
-    } 
-    else if(number_buffer[0] > number_buffer[1]){
-      std::swap(number_buffer[0], number_buffer[1]);
-    } 
-    
-    if(number_buffer[2] < number_buffer[3]){
-    
-    }
-    else if(number_buffer[2] > number_buffer[3]){
-      std::swap(number_buffer[2], number_buffer[3]);
-    }
+  //Checks the last two numbers.
+  if(number_buffer[2] < number_buffer[3]){} 
+  else if(number_buffer[2] > number_buffer[3]){ std::swap(number_buffer[2], number_buffer[3]); }
 
-    if(number_buffer[0] < number_buffer[3]){
-    
-    }
-    else if(number_buffer[0] > number_buffer[3]){
-      std::swap(number_buffer[0], number_buffer[3]);
-    }
-
-    if(number_buffer[1] < number_buffer[1]){
-    
-    }
-    else if(number_buffer[1] > number_buffer[2]){
-      std::swap(number_buffer[1], number_buffer[2]);
-    }
-  }
 
   for(int i = 0; i < number_buffer.size(); i++)
     end_num += std::to_string(number_buffer[i]);
-    std::cout << end_num << std::endl;
+    std::cout << "Your sorted numbers are: " << end_num << std::endl;
 
   return 0;
 }
